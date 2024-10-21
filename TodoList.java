5bb510e075c62e7fc30b864ea415a6a02a96567f
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2024-10-21 09:50:43 +0300

    finishing task logic

---
+++ TodoList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class TodoList {
+    private ArrayList<Task> tasks;
+    Task disc=new Task(null);
+    String discription= disc.getDescription();
+    public void  ToDoist() {
+        tasks = new ArrayList<>();
+    }
+   public void addTask(String discription ){
+      tasks.add(new Task(discription));
+      System.out.println("Task added successfully");
+    
+   }
+   public void viewTask(){
+    if (tasks.isEmpty()) {
+        System.out.println("No tasks available.");
+   }
+   else {
+      for (int i =0;i<tasks.size();i++){
+         System.out.println((i+1)+"."+tasks.get(i));
+      }
+
+   }
+
+} 
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.remove(index);
+            System.out.println("Task removed!");
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+    public void markTaskAsCompleted(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markAsCompleted();
+            System.out.println("Task marked as completed!");
+        } else {
+            System.out.println("Invalid task number.");
+        }
+    }
+}
